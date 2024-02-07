f8562ffb11efb15780cd1c59a391a3f4a072c590
Yogesh Khutwad <152191391+YogeshK34@users.noreply.github.com>
2024-02-07 07:55:09 +0530

    Add files via upload

---
+++ sample_programs/Factorial.java
@@ -0,0 +1,16 @@
+package sample_programs;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number you want factorial of: ");
+        int num = sc.nextInt();
+        int factorial = 1;
+        for(int i = 1; i <= num;i++){
+            factorial = factorial * i;
+        }
+        System.out.print("The factorial of "+num+" is "+factorial);
+    }
+}
---
+++ sample_programs/perfect_number.java
@@ -0,0 +1,34 @@
+package sample_programs;
+
+import java.util.Scanner;
+
+public class perfect_number {
+    public static void main(String[] args) {
+        /*perfect number  = sum of factors = that_number
+        for example facors of 6 are : 1 2 3 and addition of 1 + 2 + 3 = 6 henceforth 6 is a perfect number
+        * */
+//        Scanner sc = new Scanner(System.in);
+//        int number = sc.nextInt();
+//        for(int i = 1; i <=number; i++){
+//            if(number % i == 0){
+//                System.out.print(i+" ");
+//            }
+//        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        if (sum == number) {
+            System.out.println(number + " is a perfect number.");
+        } else {
+            System.out.println(number + " is not a perfect number.");
+        }
+    }
+}
