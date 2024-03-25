92d7d46a17a0c8bb5ac1615e5c0a7d5fb7eb7770
Yogesh Khutwad <152191391+YogeshK34@users.noreply.github.com>
2024-03-25 15:03:38 +0530

    Add files via upload

---
+++ leetcode/MissingNumber.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {4, 0, 2, 1};
+        missingNumber(arr);
+    }
+    static int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+        //search for missing  number
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index) {
+                return index;
+            }
+        }
+        return nums.length;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
