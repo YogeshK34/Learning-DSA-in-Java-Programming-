bfc4db2f358c1ae20dfa23e9f510d9edabcb85b5
Yogesh Khutwad <152191391+YogeshK34@users.noreply.github.com>
2024-04-04 23:30:10 +0530

    Add files via upload

---
+++ String/Firstprogram.java
@@ -0,0 +1,10 @@
+package com.String;
+
+public class Firstprogram {
+    public static void main(String[] args) {
+        String str = new String("abc");
+        String str1 = new String("abc");
+        String str2 = "abc";
+        System.out.println(str2 == str1);
+    }
+}
