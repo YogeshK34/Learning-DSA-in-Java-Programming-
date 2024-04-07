75f862531d2db2dc3e6dfce109239d651603f343
Yogesh Khutwad <152191391+YogeshK34@users.noreply.github.com>
2024-04-07 18:17:06 +0530

    Add files via upload

---
+++ String/PrettyPrinting.java
@@ -0,0 +1,13 @@
+package com.String;
+
+import com.arrays.Array_list;
+public class PrettyPrinting {
+    public static void main(String[] args) {
+     float f = 123.4321f;
+        String ans  = new Integer(67) + " " + new Array_list();
+        System.out.println(ans);
+        String ab = "ab";
+        String ba = "ab";
+        System.out.println(ab.equals(ba));
+    }
+}
